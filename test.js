6fd589d6dbb5ef6b2d790c9a0df91d1e8e50ab9a
Marek Materzok <tilk@tilk.eu>
2021-02-08 14:53:30 +0100

    Bump dependencies

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ const fix = new tester.TestFixture({i: 1}, {o: 1});
 beforeAll(() => tester.synthFixture(fix, filename));
 
 fix.testInterface();
-//fix.testFun(s => ({o: s.i}));
+fix.testPrimitives(['gates']);
+fix.testFun(s => ({o: s.i}));
 fix.testCriticalPathAcyclic(20);
 
